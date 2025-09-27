680090b54b5d5d54e3b34ae5ecbf08a587865a73
EdogawaCoder <edogawa.araujo@gmail.com>
2025-09-27 13:11:20 -0300

    created ts from the dashboard feature

--- src/app/components/pages/dashboard/dashboard.ts
+++ src/app/components/pages/dashboard/dashboard.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class Dashboard {
 
+
+  private http = inject(HttpClient);
+
+
+  ngOnInit() {
+
+    this.http.get('http://localhost:8082/api/v1/dashboard/category-quantity')
+      .subscribe((res: any) => {
+      console.table(res);
+    })
+
+    this.http.get('http://localhost:8082/api/v1/dashboard/category-avg-price')
+      .subscribe((res: any) => {
+      console.table(res);
+    })
+      
+  }
 }
