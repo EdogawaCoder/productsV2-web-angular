b31f7c67a80766a03a7c7e4b1e11ef9baadccada
EdogawaCoder <edogawa.araujo@gmail.com>
2025-10-11 13:18:19 -0300

    html and ts of search's page for exhibithion and delete function.

--- src/app/components/pages/search-products/search-products.ts
+++ src/app/components/pages/search-products/search-products.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-search-products',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './search-products.html',
   styleUrl: './search-products.css'
 })
 export class SearchProducts {
 
+  private http: HttpClient = inject(HttpClient);
+
+  products = signal<any[]>([]);
+
+
+  ngOnInit(): void {
+
+    this.http.get('http://localhost:8080/api/v1/products')
+      .subscribe((res) => {
+        this.products.set(res as any[]);
+      })
+  }
+
+   
+  deleteProduct(id: string) {
+    if (confirm('Are you sure you want to delete this product?')) {
+      this.http.delete(`http://localhost:8080/api/v1/products/` + id,{responseType: 'text'})
+        .subscribe((res) => {
+          alert(res);
+          this.ngOnInit();
+        })
+    }
+  }
 }
